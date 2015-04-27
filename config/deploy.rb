3275d884a3599a849d23349b430670a7ed43fda7
Andrej Antas <cheaterblue@gmail.com>
2015-04-27 23:48:27 +0200

    add shared folders to deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,13 +14,12 @@ set :passenger_restart_with_sudo, true
 
 set :keep_releases, 3
 
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
 namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      # nothing here
     end
   end
 end
